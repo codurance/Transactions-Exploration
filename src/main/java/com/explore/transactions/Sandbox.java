e869955a1ccc61d110061c85458ba5b62c67512b
Florian Kempenich <Flori@nKempenich.com>
2022-11-17 18:12:34 +0000

    Some more experiment w/ EMR DB

--- src/main/java/com/explore/transactions/Sandbox.java
+++ src/main/java/com/explore/transactions/Sandbox.java
@@ -3,8 +3,12 @@ package com.explore.transactions;
 import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
 import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,29 +24,84 @@ public class Sandbox {
 
     System.out.println("In sandbox");
 
+
     List<SimpleItem> items = jdbcTemplate.query(
         "select * from items LIMIT 20",
-        (rs, rowNum) -> new SimpleItem(
-            rs.getInt("itemID"),
-            rs.getString("itemName"),
-            rs.getString("itemDesc")
-        ));
+        new SimpleItemRowMapper()
+    );
+
 
     items.forEach(System.out::println);
+
+    /* Next steps
+     * ----------
+     * - Make an insert
+     *
+     * asdf
+      Next steps:
+
+    */
+
+    String vmid = UUID.randomUUID().toString();
+    int something = jdbcTemplate.update(
+        new SimpleItem(1, vmid, "Hello", "Hi").insertQueryWithoutId()
+    );
+    System.out.println("something = " + something);
+
+    List<SimpleItem> itemsAfterInsert = jdbcTemplate.query(
+        "select * from items where itemName = 'Hello'",
+        new SimpleItemRowMapper()
+    );
+    itemsAfterInsert.forEach(System.out::println);
+
   }
 
+}
+
+class SimpleItemRowMapper implements RowMapper<SimpleItem> {
+
+  @Override
+  public SimpleItem mapRow(ResultSet rs, int rowNum) throws SQLException {
+    return new SimpleItem(
+        rs.getInt("itemID"),
+        rs.getString("vmid"),
+        rs.getString("itemName"),
+        rs.getString("itemDesc")
+    );
+  }
 }
 
 class SimpleItem {
 
   public final int id;
+  public final String vmid;
   public final String name;
   public final String description;
 
-  public SimpleItem(int id, String name, String description) {
+  public SimpleItem(int id, String vmid, String name, String description) {
     this.id = id;
+    this.vmid = vmid;
     this.name = name;
     this.description = description;
+  }
+
+  public String insertQuery() {
+    return String.format(
+        "INSERT into items (itemID, vmid, itemName, itemDesc) VALUES (%d, '%s', '%s', '%s')",
+        id,
+        vmid,
+        name,
+        description
+    );
+  }
+
+  public String insertQueryWithoutId() {
+    return String.format(
+        "INSERT into items (vmid, itemName, itemDesc) VALUES ('%s', '%s', '%s')",
+        vmid,
+        name,
+        description
+    );
   }
 
   @Override
