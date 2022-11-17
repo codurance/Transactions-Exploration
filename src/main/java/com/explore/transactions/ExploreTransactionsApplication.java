7818b08f621b76534cd99d77068b06ea0b301d5f
Florian Kempenich <Flori@nKempenich.com>
2022-11-17 14:41:45 +0000

    Import project from spring initializr (& tweak dependencies)

---
+++ src/main/java/com/explore/transactions/ExploreTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.explore.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/explore/transactions/ExploreTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explore.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
