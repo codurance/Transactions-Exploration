4d260141050e2643bb234d54c902ae2ca83fe552
Florian Kempenich <Flori@nKempenich.com>
2022-11-17 15:10:29 +0000

    Turn into CLI app

--- src/main/java/com/explore/transactions/ExploreTransactionsApplication.java
+++ src/main/java/com/explore/transactions/ExploreTransactionsApplication.java
@@ -1,13 +1,18 @@
 package com.explore.transactions;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExploreTransactionsApplication {
+public class ExploreTransactionsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExploreTransactionsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		System.out.println("Hello\uD83D\uDE03 ");
+	}
 }
