40649658f51cce5943aa8a8dac277e29e6cf65df
Florian Kempenich <Flori@nKempenich.com>
2022-11-18 12:42:21 +0000

    Remove sensitive data (repo will be cleaned after this commit)

--- src/main/java/com/explore/transactions/config/DataSourceConfig.java
+++ src/main/java/com/explore/transactions/config/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package com.explore.transactions.config;
 
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,14 +9,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataSourceConfig {
 
+  // Add to .env file
+  @Value("${env.JDBC_URL}")
+  String jdbcUrl;
+  @Value("${env.JDBC_USER}")
+  String jdbcUsername;
+  @Value("${env.JDBC_PASS}")
+  String jdbcPassword;
+
+
+
   @Bean
   public DataSource dataSource() {
     return DataSourceBuilder.create()
         .driverClassName("com.mysql.cj.jdbc.Driver")
-        .url(
-            ***REMOVED***)
-        .username(***REMOVED***)
-        .password(***REMOVED***)
+        .url(jdbcUrl)
+        .username(jdbcUsername)
+        .password(jdbcPassword)
         .build();
   }
 
